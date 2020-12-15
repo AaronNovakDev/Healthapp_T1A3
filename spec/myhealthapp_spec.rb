84d912771479be3506c75de968305721529c4a0c
Aaron Novak <Aaronnovakdevelopements@gmail.com>
2020-12-16 07:05:16 +1000

    added pseudocode

---
+++ spec/myhealthapp_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../menu'
+require_relative '../'
+require_relative '../'
+require_relative '../'
+
+describe Menu do
+    it 'should retun a list of functions you wish to click on' do
+        name = "supplement"
+        menu = Menu.new
+end
+
+it 'should define a welcome method' do
+    name = "My health app"
+    menu = 
+    expect(myhealthapp.welcome).to eq(nil)
+end
